25fd00dbe61454bbbc11f9b801d31c7670a0c46f
Arnolds Manguzis <arnold@lissen.live>
2024-01-24 10:07:45 +0000

    Starting Streak component

---
+++ components/Streak.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Streak = () => {
+    return <View style={styles.container}></View>;
+};
+
+export default Streak;
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#000",
+        width: "100%",
+        height: 25,
+    },
+});
